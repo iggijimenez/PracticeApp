1cf29e65e0d67cb43bdf2a3c594491f6ff24f2c4
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-02-25 16:17:52 -0800

    Working game view state

--- TestingApp/Edutainment.swift
+++ TestingApp/Edutainment.swift
@@ -16,7 +16,7 @@ struct Edutainment: View {
     
     @State private var isGameActive = false
     @State private var selecetedMultiplyBy = 2
-    @State private var selectedQuestionAsked = 5
+    @State private var selectedQuestionCount = 5
     @State private var questions: [Questions] = []
     @State private var currentIndex = 0
     @State private var userAnswer = ""
@@ -25,32 +25,33 @@ struct Edutainment: View {
         NavigationStack {
             VStack {
                 if isGameActive {
-                    Text("active this is for gameview")
+//                    Text("active this is for gameview")
+                    GameView(question: questions[currentIndex], userAnswer: $userAnswer, onSubmit: checkAnswer)
                 } else {
                   Text("Not active this is for settings")
-                    SettingView(startGame: startGame, selectedMultiplyBy: $selecetedMultiplyBy, selectedQuestionCount: $selectedQuestionAsked)
+                    SettingView(startGame: startGame, selectedMultiplyBy: $selecetedMultiplyBy, selectedQuestionCount: $selectedQuestionCount)
                 }
             }
         }
     }
     
     func startGame() {
-        generateQuestions()
         isGameActive = true
+        generateQuestions()
     }
     
     func generateQuestions() {
-        questions = (1...selecetedMultiplyBy).map({ _ in
+        questions = (1...selectedQuestionCount).map({ _ in
             let multiplier = Int.random(in: 1...12)
             let questionText = "\(selecetedMultiplyBy) x \(multiplier) = ?"
-            return Questions(text: questionText, answer: (multiplier * selecetedMultiplyBy))
+            return Questions(text: questionText, answer: multiplier * selecetedMultiplyBy)
         })
         currentIndex = 0
         userAnswer = ""
     }
     
     func checkAnswer() {
-        if currentIndex < selectedQuestionAsked - 1 {
+        if currentIndex < selectedQuestionCount - 1 {
             currentIndex += 1
             userAnswer = ""
         } else {
@@ -74,7 +75,7 @@ struct SettingView: View {
             Stepper("Multiply by \(selectedMultiplyBy)", value: $selectedMultiplyBy, in: 2...12)
                 .padding(.horizontal)
             
-            Stepper("Number of questions \(selectedQuestionCount)", value: $selectedQuestionCount,in: 5...20, step: 5)
+            Stepper("Number of questions \(selectedQuestionCount)", value: $selectedQuestionCount, in: 5...20, step: 5)
                 .padding(.horizontal)
             
             Button("Start Game") {
@@ -85,10 +86,19 @@ struct SettingView: View {
 }
 
 struct GameView: View {
+    let question: Questions // pass question struct
+    @Binding var userAnswer: String // user input
+    var onSubmit: () -> Void // onsumbit button
     
     var body: some View {
         VStack {
+            Text(question.text)
+                .font(.title)
+            TextField("Your answer", text: $userAnswer, onCommit: onSubmit)
             
+            Button("submit") {
+                onSubmit()
+            }
         }
     }
 }
