c0d848b0e14340499c321ba121a6c2e7d4d34506
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-03-04 11:05:39 -0800

    completed, removed comments and cleaned up code

--- TestingApp/WeSplit.swift
+++ TestingApp/WeSplit.swift
@@ -8,21 +8,17 @@
 import SwiftUI
 
 struct WeSplit: View {
-    // price before tip
     @State private var price = 0.0
-    // percent amount
-    let tipAmount = [0, 10, 15, 20]
     @State private var tipSelected = 0
-    // number of people
     @State private var numberOfPeople = 0
-    // total amount
+    
+    let tipAmount = [0, 10, 15, 20]
+    
     var totalAmount: Double {
         let peopleAmount = Double(numberOfPeople) + 2
         let tipAmount = Double(tipSelected)
         
-//        return 0.0
         return (price + (price / 100 * tipAmount)) / peopleAmount
-//        return (checkAmount + (checkAmount / 100 * tipSelection)) / peopleCount
     }
     
     var body: some View {
@@ -50,10 +46,11 @@ struct WeSplit: View {
                     .pickerStyle(.segmented)
                 }
                 
-                Section("total amount") {
+                Section("total amount with tip") {
                     Text(totalAmount, format: .currency(code: "USD"))
                 }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
