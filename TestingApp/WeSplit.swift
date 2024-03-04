dd20cd001b836ab84b77921c40aea75037631b6d
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-03-04 11:00:00 -0800

    added total amount with tip

--- TestingApp/WeSplit.swift
+++ TestingApp/WeSplit.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct WeSplit: View {
     // price before tip
-    @State private var price = 0
+    @State private var price = 0.0
     // percent amount
     let tipAmount = [0, 10, 15, 20]
     @State private var tipSelected = 0
     // number of people
     @State private var numberOfPeople = 0
     // total amount
-    @State private var totalAmount = 0
+    var totalAmount: Double {
+        let peopleAmount = Double(numberOfPeople) + 2
+        let tipAmount = Double(tipSelected)
+        
+//        return 0.0
+        return (price + (price / 100 * tipAmount)) / peopleAmount
+//        return (checkAmount + (checkAmount / 100 * tipSelection)) / peopleCount
+    }
     
     var body: some View {
         NavigationStack {
@@ -34,7 +41,6 @@ struct WeSplit: View {
                     }
                 }
                 
-                
                 Section("Tip Percentage") {
                     Picker("Tip", selection: $tipSelected) {
                         ForEach(tipAmount, id: \.self) {
@@ -42,6 +48,10 @@ struct WeSplit: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                }
+                
+                Section("total amount") {
+                    Text(totalAmount, format: .currency(code: "USD"))
                 }
             }
         }
